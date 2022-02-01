0d754a03e238716232f19546e164d892389d6a18
logs-in-my-eyes69 <rowanrothe@icloud.com>
2022-02-01 18:57:48 -0500

    updated server and client stuff. i dont remember

--- serverside/server.js
+++ serverside/server.js
@@ -1,10 +1,16 @@
-const io = require("socket.io")(3000);
+const io = require("socket.io")(3000, {
+    //set this so cors doesnt block connection to the server
+    cors: {
+        origin: ['http://localhost:8080'],
+    },
+});
 
 console.log("Server started");
 
 io.on("connection", socket => {
     socket.on("join", code => {
         socket.join(code);
+        socket.emit('joined', code);
         console.log(`Client ${socket.id} joined room ${code}`);
     });
 });
