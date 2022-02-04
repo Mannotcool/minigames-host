8256223c1e83cf1716f7bbc9823daa1a8f007a23
logs-in-my-eyes69 <rowanrothe@icloud.com>
2022-02-03 19:10:55 -0500

    idk what i changed

--- serverside/server.js
+++ serverside/server.js
@@ -2,29 +2,28 @@ const io = require("socket.io")(3000, {
     //set this so cors doesnt block connection to the server
     cors: {
         origin: ['http://127.0.0.1:5500']
-    },
+    }
 });
 
 
 
 var rooms = [];
 
+console.log("Server started");
+
 io.on("connection", socket => {
-    console.log("Server started");
     socket.on("roomQuery", code => {
         for (var i = 0; i < rooms.length; i++) {
             if (rooms[i][0] == code) {
-                //true if room exists. else false
+                //true if room exists then join. else false and create room for client
                 socket.join(code);
                 io.to(socket.id).emit('roomQueryResp', true);
                 console.log("Client "+socket.id+" joined room "+code);
                 return;
             }
         }
-        io.to(socket.id).emit('roomQueryResp', false);
-    });
 
-    socket.on("roomCreate", code => {
+        io.to(socket.id).emit('roomQueryResp', false);
         socket.join(code);
         rooms.push([code, socket.id]);
         console.log("Client "+socket.id+" created room "+code);
@@ -33,7 +32,8 @@ io.on("connection", socket => {
     socket.on("disconnect", () => {
         for (var i = 0; i < rooms.length; i++) {
             if (rooms[i][1] == socket.id) {
-                console.log("Client "+socket.id+" left room "+rooms[i][0]);
+                io.socketsLeave(rooms[i][0]);
+                console.log("Client "+socket.id+" destroyed room "+rooms[i][0]);
                 rooms.splice(i, 1);
                 return;
             }
